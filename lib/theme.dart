1deca3c314c296b7b1eaf2163da08bcdcbd5616f
Praks310 <hafiyyan235@gmail.com>
2022-01-12 13:35:21 +0700

    awal commit

---
+++ lib/theme.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color bgcolor = Color(0xffE5E5E5);
+Color greyColor = Color(0xff82868E);
+Color whiteColor = Color(0xffFFFFFF);
+Color purpleColor = Color(0xff5843BE);
+Color orangeColor = Color(0xffFF9376);
+Color blackColor = Color(0xff000000);
+
+double edge = 24;
+
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: blackColor,
+);
+
+TextStyle whiteTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: whiteColor,
+);
+
+TextStyle greyTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w300,
+  color: greyColor,
+);
+
+TextStyle purpleTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: purpleColor,
+);
+
+TextStyle regulerTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w400,
+  color: blackColor,
+);
